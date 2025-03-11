2b4e16b93c70dbcd6608f75b96870ba0c6e62be6
Dmitriy Gerasimov <up@websector.ru>
2025-03-11 12:56:47 +0300

    fix: xver 1

--- src/modules/panel/vless-configuration.sh
+++ src/modules/panel/vless-configuration.sh
@@ -65,7 +65,7 @@ vless_configuration() {
         "realitySettings": {
           "dest": "127.0.0.1:$SELF_STEAL_PORT",
           "show": false,
-          "xver": 0,
+          "xver": 1,
           "shortIds": [
             "$short_id"
           ],
