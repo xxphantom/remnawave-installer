a257d9fdc1087158ab4d8c3ba27a49154443ea51
Dmitriy Gerasimov <up@websector.ru>
2026-08-01 21:26:33 +0300

    chore: bump version to 2.3.0

--- src/lib/constants.sh
+++ src/lib/constants.sh
@@ -56,7 +56,7 @@ YELLOW=$(tput setaf 3)
 NC=$(tput sgr0)
 
 # Script version
-VERSION="2.2.0"
+VERSION="2.3.0"
 
 # Docker image tags based on branch
 # Check if branch is a version number (e.g., 1.65, 2.0.1)
